41497b351138a133ac8a4db9cf6d4ef0cca3e49a
JaeHyun <wogus0518@naver.com>
2022-12-19 09:50:05 +0900

    project init

---
+++ src/main/java/com/ll/exam/app__2022_12_19_1/App202212191Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_12_19_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App202212191Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App202212191Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app__2022_12_19_1/App202212191ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app__2022_12_19_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App202212191ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
